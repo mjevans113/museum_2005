b04f941062b7d47e1305d2f47a51a563f7ac8aba
Michael Evans <michaeljevans113@gmail.com>
2020-05-27 09:17:50 -0600

    Add attribute testing

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -8,4 +8,11 @@ class PatronTest < MiniTest::Test
 
     assert_instance_of Patron, patron_1
   end
+
+  def test_it_has_readable_attributes
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal 'Bob', patron_1.name
+    assert_equal 20, patron_1.spending_money
+  end
 end
