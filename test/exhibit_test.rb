24b88f514bb4fe9ee652bcdbcdde5f724bc08012
Michael Evans <michaeljevans113@gmail.com>
2020-05-27 09:12:46 -0600

    Add attribute testing

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -8,4 +8,11 @@ class ExhibitTest < MiniTest::Test
 
     assert_instance_of Exhibit, exhibit
   end
+
+  def test_it_has_readable_attributes
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_equal 'Gems and Minerals', exhibit.name
+    assert_equal 0, exhibit.cost
+  end
 end
