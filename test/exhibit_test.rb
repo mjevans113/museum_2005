d385d35a93c66787dc4d93356177b1f2de340491
Michael Evans <michaeljevans113@gmail.com>
2020-05-27 09:10:14 -0600

    Create test suite for Exhibit class

---
+++ test/exhibit_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+
+class ExhibitTest < MiniTest::Test
+  def test_it_exists
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_instance_of Exhibit, exhibit
+  end
+end
