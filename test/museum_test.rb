7547e1806faaffc28b25b3c743251b925183a64d
Michael Evans <michaeljevans113@gmail.com>
2020-05-27 09:48:11 -0600

    Add patron admission test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -16,6 +16,7 @@ class MuseumTest < MiniTest::Test
 
     assert_equal 'Denver Museum of Nature and Science', dmns.name
     assert_equal [], dmns.exhibits
+    assert_equal [], dmns.patrons
   end
 
   def test_it_can_add_exhibits
@@ -50,5 +51,18 @@ class MuseumTest < MiniTest::Test
 
     assert_equal [gems_and_minerals, dead_sea_scrolls], dmns.recommend_exhibits(patron_1)
     assert_equal [imax], dmns.recommend_exhibits(patron_2)
+  end
+
+  def test_it_can_admit_patrons
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    patron_1 = Patron.new("Bob", 20)
+    patron_2 = Patron.new("Sally", 20)
+
+    dmns.admit(patron_1)
+    assert_equal [patron_1], dmns.patrons
+
+    dmns.admit(patron_2)
+    assert_equal [patron_1, patron_2], dmns.patrons
   end
 end
