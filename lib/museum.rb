76b486828b404365eb51287ceef13e6f0a06598f
Michael Evans <michaeljevans113@gmail.com>
2020-05-27 10:30:03 -0600

    Add ticket_lottery_contestants method

--- lib/museum.rb
+++ lib/museum.rb
@@ -21,4 +21,22 @@ class Museum
   def admit(patron)
     @patrons << patron
   end
+
+  # def patrons_by_exhibit_interest
+  #   exhibit_interest = {}
+  #   @exhibits.each do |exhibit|
+  #     exhibit_interest[exhibit] = []
+  #   end
+  #   @patrons.each do |patron|
+  #
+  #   end
+  # end
+
+  def ticket_lottery_contestants(exhibit)
+    contestants = []
+    @patrons.each do |patron|
+      contestants << patron if patron.interests.include?(exhibit.name) && patron.spending_money < exhibit.cost
+    end
+    contestants
+  end
 end
