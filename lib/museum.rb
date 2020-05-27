95e3eaa6b52a77a4bd264528a1f3734aa540179f
Michael Evans <michaeljevans113@gmail.com>
2020-05-27 11:00:23 -0600

    Current progress

--- lib/museum.rb
+++ lib/museum.rb
@@ -22,15 +22,15 @@ class Museum
     @patrons << patron
   end
 
-  # def patrons_by_exhibit_interest
-  #   exhibit_interest = {}
-  #   @exhibits.each do |exhibit|
-  #     exhibit_interest[exhibit] = []
-  #   end
-  #   @patrons.each do |patron|
-  #
-  #   end
-  # end
+  def patrons_by_exhibit_interest
+    exhibit_interest = {}
+    @exhibits.each do |exhibit|
+      @patrons.each do |patron|
+        exhibit_interest[exhibit] += patron if patron.interests.include?(exhibit)
+      end
+    end
+    exhibit_interest
+  end
 
   def ticket_lottery_contestants(exhibit)
     contestants = []
@@ -38,5 +38,13 @@ class Museum
       contestants << patron if patron.interests.include?(exhibit.name) && patron.spending_money < exhibit.cost
     end
     contestants
+  end
+
+  def draw_lottery_winner(exhibit)
+    if ticket_lottery_contestants(exhibit) == []
+      nil
+    else
+      ticket_lottery_contestants(exhibit).sample.name
+    end
   end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -67,30 +67,30 @@ class MuseumTest < MiniTest::Test
     assert_equal [patron_1, patron_2], dmns.patrons
   end
 
-  # def test_it_displays_patrons_by_exhibit_interest
-  #   dmns = Museum.new("Denver Museum of Nature and Science")
-  #
-  #   gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-  #   dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-  #   imax = Exhibit.new({name: "IMAX",cost: 15})
-  #
-  #   dmns.add_exhibit(gems_and_minerals)
-  #   dmns.add_exhibit(dead_sea_scrolls)
-  #   dmns.add_exhibit(imax)
-  #
-  #   patron_1 = Patron.new("Bob", 20)
-  #   patron_1.add_interest('Gems and Minerals')
-  #   patron_2 = Patron.new("Sally", 20)
-  #   patron_2.add_interest('Dead Sea Scrolls')
-  #   patron_3 = Patron.new("Johnny", 5)
-  #   patron_3.add_interest('Dead Sea Scrolls')
-  #
-  #   expected = {gems_and_minerals => [patron_1],
-  #               dead_sea_scrolls  => [patron_2, patron_3],
-  #               imax              => []}
-  #
-  #   assert_equal expected, dmns.patrons_by_exhibit_interest
-  # end
+  def test_it_displays_patrons_by_exhibit_interest
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest('Gems and Minerals')
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest('Dead Sea Scrolls')
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest('Dead Sea Scrolls')
+
+    expected = {gems_and_minerals => [patron_1],
+                dead_sea_scrolls  => [patron_2, patron_3],
+                imax              => []}
+
+    assert_equal expected, dmns.patrons_by_exhibit_interest
+  end
 
   def test_ticket_lottery_contestants_are_added
     dmns = Museum.new("Denver Museum of Nature and Science")
@@ -116,5 +116,36 @@ class MuseumTest < MiniTest::Test
 
     assert_equal [patron_3], dmns.ticket_lottery_contestants(dead_sea_scrolls)
     assert_equal [], dmns.ticket_lottery_contestants(gems_and_minerals)
+  end
+
+  def test_draw_lottery_winner
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    dmns.ticket_lottery_contestants(dead_sea_scrolls)
+
+    assert_equal patron_1.name || patron_3.name, dmns.draw_lottery_winner(dead_sea_scrolls)
+
+    dmns.ticket_lottery_contestants(imax)
+    assert_nil dmns.draw_lottery_winner(imax)
   end
 end
