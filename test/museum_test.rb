bf03e7e9e034e1c03ee84c04246f47e9f480e2f1
Michael Evans <michaeljevans113@gmail.com>
2020-05-27 10:19:51 -0600

    Add test for ticket lottery contestants

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require './lib/exhibit'
 require './lib/patron'
 require './lib/museum'
+require 'pry'
 
 class MuseumTest < MiniTest::Test
   def test_it_exists
@@ -89,5 +90,26 @@ class MuseumTest < MiniTest::Test
                 imax              => []}
 
     assert_equal expected, dmns.patrons_by_exhibit_interest
+  end
+
+  def test_ticket_lottery_contestants_are_added
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest('Gems and Minerals')
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest('Dead Sea Scrolls')
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest('Dead Sea Scrolls')
+
+    assert_equal [patron_2, patron_3], dmns.ticket_lottery_contestants(dead_sea_scrolls)
   end
 end
